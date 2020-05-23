de3645e69ca44a8f0b116c56936a971d194da0a4
ben <buckhendersonunionbuster23@gmail.com>
2020-05-22 23:09:24 -0700

    functional in a hello world state; input seems a big jagged

--- anal_multiplex.cpp
+++ anal_multiplex.cpp
@@ -7,8 +7,23 @@ int analReadVal;
 int addressA = 27;
 int addressB = 29;
 int addressC = 31;
+int A;
+int B;
+int C;
 
 int nDials = 2;
+int nRows = 2; // this value refers to how many states the dials can exist in
+
+int encoderPin1 = 20;
+int encoderPin2 = 21;
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
 
 void setup() {
   //Serial.begin(38400);
@@ -19,6 +34,17 @@ void setup() {
   pinMode(addressC, OUTPUT);
   // Prepare read pins
   pinMode(multiplexDialIn, INPUT);
+
+  pinMode(encoderPin1, INPUT);
+  pinMode(encoderPin2, INPUT);
+
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+  attachInterrupt(digitalPinToInterrupt(encoderPin1), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPin2), updateEncoder, CHANGE);
 }
 
 void writeMux(int i){
@@ -35,8 +61,32 @@ void writeMux(int i){
 void loop() {
   for (int i = 0; i < nDials; i++){
   writeMux(i);
-  analReadVal = analogRead(multiplexDialIn);
+  analReadVal = analogRead(multiplexDialIn) * encoderValue;
   Serial.println(analReadVal);
+  Serial.print("Encoder value: ");
+  Serial.println(encoderValue);
   }
   delay(200);
+}
+
+void updateEncoder(){
+  Serial.println("running updateEncoder()");
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
+    encoderValue ++;
+    Serial.print("encoderValue in loop 0");
+    Serial.println(encoderValue);
+    encoderValue = encoderValue % nRows;
+  }
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) {
+     encoderValue --;
+     Serial.print("encoderValue in loop 1");
+     Serial.println(encoderValue);
+     encoderValue = max(0, encoderValue);
+  }
+  lastEncoded = encoded; //store this value for next time
 }
